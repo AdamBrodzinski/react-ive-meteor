d2026a19dfb738df3508f464b0f467f0764dac78
Adam Brodzinski <adambrodzinski@gmail.com>
2015-06-06 16:10:08 -0400

    adds publication security warning

--- server/publications/feed.js
+++ server/publications/feed.js
@@ -3,6 +3,21 @@
 Meteor.publish('feed', function(fields) {
   console.log('Publishing Posts', fields);
 
+  // SECURITY NOTE
+  // if this was data that could not be shown to a specific set of
+  // users or a logged out user, you would check here to verify they're
+  // allowed to receive the data.
+  // **TRUST NOTHING FROM THE CLIENT** instead ask the server what they're
+  // user ID is and check their permissions to see if the role is met, never
+  // never pass in the user ID from the client as they could guess an admin
+  // ID and gain access.
+  //
+  //  if (!this.userId)
+  //       throw new Meteor.Error(401, "Access denied, please login");
+  //        or
+  //  if (Roles.userIsInRole(this.userId, 'admin'))
+  //       throw new Meteor.Error(403, "Not authorized to view this data");
+
   if (!fields.posts || !fields.postComments._id) {
     return this.ready();
   }
