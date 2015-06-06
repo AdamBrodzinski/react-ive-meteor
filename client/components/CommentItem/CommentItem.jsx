0bd370ef4443c6f3b058053592a2d942f5f4b832
Adam Brodzinski <adambrodzinski@gmail.com>
2015-06-05 20:26:30 -0400

    adds comment styles

--- client/components/CommentItem/CommentItem.jsx
+++ client/components/CommentItem/CommentItem.jsx
@@ -4,23 +4,20 @@ this.CommentItem = React.createClass({
   render() {
     return (
       <div className="comment-item">
-        <div className="comment-item__photo">
-        </div>
+        <div className="comment-item__photo"></div>
 
         <div className="comment-item__text">
-          
+          <span className="comment-item__name">
+            { this.props.username }
+          </span>
 
-        <span className="comment-item__name">
-          { this.props.username }
-        </span>
+          <span className="comment-item__desc">
+            { this.props.desc }
+          </span>
 
-        <span className="comment-item__desc">
-          { this.props.desc }
-        </span>
-
-        <div className="comment-item__date">
-          { this.props.createdAt.toDateString() }
-        </div>
+          <div className="comment-item__date">
+            { this.props.createdAt.toDateString() }
+          </div>
         </div>
       </div>
     );
