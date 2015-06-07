883875d2044b302296591106a57a1384ba228956
Adam Brodzinski <adambrodzinski@gmail.com>
2015-06-07 19:04:08 -0400

    move feedlist into Feed/ folder

---
+++ client/components/Feed/FeedList.jsx
@@ -0,0 +1,30 @@
+/*global FeedItem */
+
+this.FeedList = React.createClass({
+  // TODO break out more button into comp
+  render() {
+    console.log("[FeedList] Rendering");
+    return (
+      <div>
+        {
+          this.props.postItems.map(doc => {
+            // pull comments from MiniMongo client store
+            var comments = PostComments.find({postId: doc._id}, {sort: {createdAt: -1}}).fetch();
+
+            return <FeedItem key={doc._id}
+              { ...doc }
+              comments={ comments }
+              destroyPost={ doc.destroy }
+              createComment={ PostComment.create }
+            />;
+          })
+        }
+        <button className='more-btn'
+            onClick={this.incrementLimit}>
+          Load More
+        </button>
+      </div>
+    );
+  }
+});
+
