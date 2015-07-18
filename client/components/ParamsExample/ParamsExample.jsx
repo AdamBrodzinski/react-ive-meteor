50152892215167205068d9e0522a73643343549f
Adam Brodzinski <adambrodzinski@gmail.com>
2015-07-18 16:59:00 -0400

    clean up paramsexample

--- client/components/ParamsExample/ParamsExample.jsx
+++ client/components/ParamsExample/ParamsExample.jsx
@@ -1,27 +1,28 @@
-/*global FlowRouter, FeedStore */
+/*global FeedStore, FeedActions */
+/* jshint maxlen: false */
 
 this.ParamsExample = React.createClass({
+  handleClick() {
+    FeedActions.incrementStepParam();
+  },
+
   render() {
     return (
       <div className='params-example'>
-        <h4>
-          Reactive Query Params
-        </h4>
+        <h4>Reactive Query Params</h4>
+
         <p>
           FlowRouter provides a reactive API that will
           allows for automatic render updates when the
           data changes
         </p>
 
-        <b>Step:</b> { FeedStore.getStepParam() }
+        <b>Step:</b> { FeedStore.getStepParam() } <br/><br/>
 
-        <br /><br />
-
-        <button onClick={ FeedActions.incrementStepParam }>
+        <button onClick={ this.handleClick }>
           Press to Increment Step Query Params
         </button>
       </div>
     );
   }
 });
-
