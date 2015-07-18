e8a99f9bab7e63953913272bea0934905acb744e
Adam Brodzinski <adambrodzinski@gmail.com>
2015-07-18 18:17:51 -0400

    clean up feed item footer

--- client/components/FeedItem/FeedItemFooter.jsx
+++ client/components/FeedItem/FeedItemFooter.jsx
@@ -1,4 +1,5 @@
-/*global User */
+/*global User, PostActions, ErrorActions */
+/* jshint maxlen: false */
 
 this.FeedItemFooter = React.createClass({
   propTypes: {
@@ -6,18 +7,28 @@ this.FeedItemFooter = React.createClass({
     commentCount: React.PropTypes.number,
   },
 
+  getInitialState() {
+    return {
+      hasLiked: false
+    };
+  },
+
   // *note* doesn't check for mult. likes by same person on the backend
   handleLikeClick(e) {
     e.preventDefault();
-    if (User.loggedOut()) return ErrorActions.needLogin();
+    if (User.loggedOut()) {
+      ErrorActions.needLogin();
+    }
     PostActions.likePost(this.props._id);
+    this.setState({hasLiked: true});
   },
 
   render() {
     return (
       <div className="feed-item__footer">
-        <a href="#" onClick={ this.handleLikeClick }>Like</a>
-        <a href="">Comment</a>
+        {!this.state.hasLiked &&
+          <a href="#" onClick={ this.handleLikeClick }>Like</a>
+        }
 
         <span className='by-people'>
           Liked by {this.props.likeCount} people
